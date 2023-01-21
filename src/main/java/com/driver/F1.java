622947079db4e01ccfd595ea88c3e77fbb9a49e1
bank app <saurabhsngh5484@gmail.com>
2023-01-21 16:40:57 +0530

    first commit by saurabh singh

--- src/main/java/com/driver/F1.java
+++ src/main/java/com/driver/F1.java
@@ -4,6 +4,7 @@ public class F1 extends Car {
 
     public F1(String name, boolean isManual) {
         //Use arbitrary values for parameters which are not mentioned
+        super(name,4,0,6,isManual,"type",1);
     }
 
     public void accelerate(int rate){
@@ -20,6 +21,32 @@ public class F1 extends Car {
 
         if(newSpeed == 0) {
             //Stop the car, set gear as 1
+            stop();
+            setCurrentGear(1);
+        }
+        else if(newSpeed >=1 && newSpeed<=50) {
+            setCurrentSpeed(newSpeed);
+            setCurrentGear(1);
+        }
+        else if(newSpeed >=51 && newSpeed<=100) {
+            setCurrentSpeed(newSpeed);
+            setCurrentGear(2);
+        }
+        else if(newSpeed >=101 && newSpeed<=150) {
+            setCurrentSpeed(newSpeed);
+            setCurrentGear(3);
+        }
+        else if(newSpeed >=151 && newSpeed<=200) {
+            setCurrentSpeed(newSpeed);
+            setCurrentGear(4);
+        }
+        else if(newSpeed >=201 && newSpeed<=250) {
+            setCurrentSpeed(newSpeed);
+            setCurrentGear(5);
+        }
+        else if(newSpeed >250) {
+            setCurrentSpeed(newSpeed);
+            setCurrentGear(6);
         }
         //for all other cases, change the gear accordingly
 
