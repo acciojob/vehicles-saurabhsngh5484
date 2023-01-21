0988f008af9a7beb942ab5ca1958e54d0b87b1e6
bank app <saurabhsngh5484@gmail.com>
2023-01-21 16:50:10 +0530

    second commit

--- src/main/java/com/driver/Car.java
+++ src/main/java/com/driver/Car.java
@@ -78,12 +78,12 @@ public class Car extends Vehicle {
         this.seats = seats;
     }
     public void changeGear(int newGear){
-
+        currentGear = newGear;
         System.out.println("changeGear method called - The gear is changed to: " + currentGear);
     }
 
     public void changeSpeed(int newSpeed, int newDirection){
-
+        move(newSpeed, newDirection);
         System.out.println("changeSpeed method called - The speed is changed to: " + newSpeed + ", and the direction is changed to: " + newDirection + " degrees");
     }
 }
